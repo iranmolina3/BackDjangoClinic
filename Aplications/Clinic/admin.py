69f0fb4939f040e1fa7ad8e9806ef2ee2788737c
DevIran <iranmolina3@gmail.com>
2020-08-10 17:37:29 -0600

    Added models EstadoCivil and Municipio

--- Aplications/Clinic/admin.py
+++ Aplications/Clinic/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Rol, EstadoCivil, Municipio
 
 # Register your models here.
+
+admin.site.register(Rol)
+admin.site.register(EstadoCivil)
+admin.site.register(Municipio)
--- Aplications/Clinic/models.py
+++ Aplications/Clinic/models.py
@@ -5,7 +5,37 @@ from django.db import models
 # Create table rol
 
 class Rol(models.Model):
-    PK_ROL = models.AutoField(primary_key = True)
+    PK_ROL = models.AutoField(primary_key=True)
     NOMBRE = models.CharField(max_length=25, blank=False, null=False)
     ESTADO = models.BooleanField(default=True, blank=False, null=False)
 
+    class Meta:
+        verbose_name = 'Rol'
+        verbose_name_plural = 'Roles'
+
+    def __str__(self):
+        return self.NOMBRE
+
+class EstadoCivil(models.Model):
+    PK_ESTADO_CIVIL = models.AutoField(primary_key=True)
+    NOMBRE = models.CharField(max_length=25, blank=False, null=False)
+    ESTADO = models.BooleanField(default=True, blank=False, null=False)
+
+    class Meta:
+        verbose_name = 'Estado Civil'
+        verbose_name_plural = 'Estados Civiles'
+
+    def __str__(self):
+        return self.NOMBRE
+
+class Municipio(models.Model):
+    PK_MUNICIPIO = models.AutoField(primary_key=True)
+    NOMBRE = models.CharField(max_length=25, blank=False, null=False)
+    ESTADO = models.BooleanField(default=True, blank=False, null=False)
+
+    class Meta:
+        verbose_name = 'Municipio'
+        verbose_name_plural = 'Municipios'
+
+    def __str__(self):
+        return self.NOMBRE
