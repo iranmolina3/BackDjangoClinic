495f9af1ef28509e28008fbd621c380b12d1f8c3
DevIran <iranmolina3@gmail.com>
2020-08-10 15:03:08 -0600

    Maked migrations and model rol

--- Aplications/Clinic/models.py
+++ Aplications/Clinic/models.py
@@ -2,3 +2,10 @@ from django.db import models
 
 # Create your models here.
 
+# Create table rol
+
+class Rol(models.Model):
+    PK_ROL = models.AutoField(primary_key = True)
+    NOMBRE = models.CharField(max_length=25, blank=False, null=False)
+    ESTADO = models.BooleanField(default=True, blank=False, null=False)
+
