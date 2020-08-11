250af744cae9697160a7e8b8c05b7820dd6783bd
DevIran <iranmolina3@gmail.com>
2020-08-11 17:29:07 -0600

    Added models in admin django

--- Aplications/Clinic/admin.py
+++ Aplications/Clinic/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Rol, EstadoCivil, Municipio, Direccion, Persona, \
-                    Pregunta, Usuario, HistorialCsat, TipoCita
+                    Pregunta, Usuario, HistorialCsat, TipoCita, Cita, ExamenFisico, \
+                    Consulta, Antecedente
 
 # Register your models here.
 
@@ -13,3 +14,7 @@ admin.site.register(Pregunta)
 admin.site.register(Usuario)
 admin.site.register(HistorialCsat)
 admin.site.register(TipoCita)
+admin.site.register(Cita)
+admin.site.register(ExamenFisico)
+admin.site.register(Consulta)
+admin.site.register(Antecedente)
