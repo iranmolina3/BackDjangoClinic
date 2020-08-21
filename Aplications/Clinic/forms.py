13612d8405988a60ff8fd6d3ff1a04e1f3f803f0
DevIran <iranmolina3@gmail.com>
2020-08-20 22:29:58 -0600

    Change forms quit primary_key

--- Aplications/Clinic/forms.py
+++ Aplications/Clinic/forms.py
@@ -5,82 +5,82 @@ class formRol(forms.ModelForm):
     class Meta:
         model = Rol
         fields = [
-            'PK_ROL', 'NOMBRE', 'ESTADO'
+            'NOMBRE', 'ESTADO'
         ]
 
 class formEstadoCivil(forms.ModelForm):
     class Meta:
         model = [
-            'PK_ESTADO_CIVIL', 'NOMBRE', 'ESTADO'
+            'NOMBRE', 'ESTADO'
         ]
 
 class formMunicipio(forms.ModelForm):
     class Meta:
         model = [
-            'PK_MUNICIPIO', 'NOMBRE', 'ESTADO'
+            'NOMBRE', 'ESTADO'
         ]
 
 class formDireccion(forms.ModelForm):
     class Meta:
         model = [
-            'PK_DIRECCION', 'DESCRIPCION', 'ESTADO', 'FK_MUNICIPIO'
+            'DESCRIPCION', 'ESTADO', 'FK_MUNICIPIO'
         ]
 
 class formPersona(forms.ModelForm):
     class Meta:
         model = [
-            'PK_PERSONA', 'NOMBRE', 'APELLIDO', 'DPI', 'EDAD', 'FECHA', 'TELEFONO', 'GENERO',
+            'NOMBRE', 'APELLIDO', 'DPI', 'EDAD', 'FECHA', 'TELEFONO', 'GENERO',
             'ESTADO', 'FK_DIRECCION', 'FK_ESTADO_CIVIL'
         ]
 
 class formPregunta(forms.ModelForm):
     class Meta:
         model = [
-            'PK_PREGUNTA', 'DESCRIPCION', 'FECHA_CREACION', 'ESTADO'
+            'DESCRIPCION', 'FECHA_CREACION', 'ESTADO'
         ]
 
 class formUsuario(forms.ModelForm):
     class Meta:
         model = [
-            'PK_USUARIO', 'CARNET', 'CONTRASENIA', 'FECHA_CREACION', 'CORREO', 'ESTADO',
+            'CARNET', 'CONTRASENIA', 'FECHA_CREACION', 'CORREO', 'ESTADO',
             'FK_PERSONA', 'FK_ROL'
         ]
 
 class formHistorialCsat(forms.ModelForm):
     class Meta:
         model = [
-            'PK_HISTORIAL_CSAT', 'RESPUESTA', 'FECHA_CREACION', 'ESTADO', 'FK_PREGUNTA'
+            'RESPUESTA', 'FECHA_CREACION', 'ESTADO', 'FK_PREGUNTA'
         ]
 
 class formTipoCita(forms.ModelForm):
     class Meta:
         model = [
-            'PK_TIPO_CITA', 'NOMBRE', 'ESTADO'
+            'NOMBRE', 'ESTADO'
         ]
 
 class formCita(forms.ModelForm):
     class Meta:
         model = [
-            'PK_CITA', 'NUMERO', 'FECHA_CREACION', 'FECHA_FINALIZACION', 'ESTADO'
+            'NUMERO', 'FECHA_CREACION', 'FECHA_FINALIZACION', 'ESTADO'
         ]
 
 class formConsulta(forms.ModelForm):
     class Meta:
         model = [
-            'PK_CONSULTA', 'MOTIVO_CONSULTA', 'HISTORIA', 'ESTADO'
+            'MOTIVO_CONSULTA', 'HISTORIA', 'ESTADO'
         ]
 
 class formExamenFisico(forms.ModelForm):
     class Meta:
         model = [
-            'PK_EXAMEN_FISICO', 'PRESION_ARTERIAL', 'FRECUENCIA_CARDIACA', 'FRECUENCIA_RESPIRATORIA',
+            'PRESION_ARTERIAL', 'FRECUENCIA_CARDIACA', 'FRECUENCIA_RESPIRATORIA',
             'TEMPERATURA', 'FRECUENCIA_CARDIACA_FETAL', 'IMPRESION_CLINCIA', 'ESTADO'
         ]
 
 class formAntecedente(forms.ModelForm):
     class Meta:
         model = [
-            'PK_ANTECEDENTES', 'ULTIMA_REGLA', 'FECHA_PROBABLE_PARTO', 'GESTA', 'ABORTO', 'HIJOS_VIVOS',
+            'ULTIMA_REGLA', 'FECHA_PROBABLE_PARTO', 'GESTA', 'ABORTO', 'HIJOS_VIVOS',
             'PESO', 'QUIRURGICO', 'MEDICO', 'ALERGIA', 'FAMILIAR', 'HABITO', 'CIGARRO', 'LICOR', 'ESTADO'
         ]
 
