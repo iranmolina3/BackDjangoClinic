cf0a55d5bc6df088df5abdd0f12ea9ec2095cfab
DevIran <iranmolina3@gmail.com>
2020-09-20 12:19:55 -0600

    recover conf dababase production

--- DevBackDjangoClinic/settings.py
+++ DevBackDjangoClinic/settings.py
@@ -93,7 +93,6 @@ WSGI_APPLICATION = 'DevBackDjangoClinic.wsgi.application'
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #    }
-
 #}
 
 import dj_database_url
