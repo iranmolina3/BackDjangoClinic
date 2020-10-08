3a36bed329af16abe3aba4bba6cd2053bc1139b3
DevIran <iranmolina3@gmail.com>
2020-10-08 10:16:33 -0600

    new changes

--- DevBackDjangoClinic/settings.py
+++ DevBackDjangoClinic/settings.py
@@ -78,31 +78,31 @@ WSGI_APPLICATION = 'DevBackDjangoClinic.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-#DATABASES = {
+DATABASES = {
 
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#        'NAME': 'doctor_fernando',
-#        'USER': 'IranDev',
-#        'PASSWORD': '12345',
-#        'HOST': 'localhost',
-#        'PORT': 5432
-#    }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'doctor_fernando',
+        'USER': 'IranDev',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': 5432
+    }
 
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #    }
-#}
+}
 
 import dj_database_url
 from decouple import config
 
-DATABASES = {
-    'default':dj_database_url.config(
-        default=config('DATABASE_URL')
-   )
-}
+#DATABASES = {
+#    'default':dj_database_url.config(
+#        default=config('DATABASE_URL')
+#   )
+#}
 
 
 
