1600b2efab57f0ee5fab7ff9b46769637d035a07
DevIran <iranmolina3@gmail.com>
2020-11-07 18:43:57 -0600

    change dir dasabase to production

--- DevBackDjangoClinic/settings.py
+++ DevBackDjangoClinic/settings.py
@@ -78,31 +78,31 @@ WSGI_APPLICATION = 'DevBackDjangoClinic.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
+#DATABASES = {
 
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'DBClinic',
-        'USER': 'DBAIran',
-        'PASSWORD': '12345',
-        'HOST': '127.0.0.1',
-        'PORT': 5432
-    }
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'DBClinic',
+#        'USER': 'DBAIran',
+#        'PASSWORD': '12345',
+#        'HOST': '127.0.0.1',
+#        'PORT': 5432
+#    }
 
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #    }
-}
+#}
 
 import dj_database_url
 from decouple import config
 
-#DATABASES = {
-#    'default':dj_database_url.config(
-#        default=config('DATABASE_URL')
-#   )
-#}
+DATABASES = {
+    'default':dj_database_url.config(
+        default=config('DATABASE_URL')
+   )
+}
 
 
 
